312ca8d07f6025776fbafd7898d164879d493306
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-01 19:39:03 +0300

    add EX5

---
+++ EX5/Program.cs
@@ -0,0 +1,59 @@
+﻿void Fillarray(int [] collection)
+    {
+int length = collection. Length;
+int index = 0;
+while (index < length)
+{
+    collection [index] = new Random() .Next (1, 10);
+
+    //index = index + 1;
+    index ++;
+}
+    }
+
+
+void Printarray(int [] col)
+{
+int count = col. Length;
+int position1 = 0;
+
+while (position1 < count)
+{Console.WriteLine(col[position1]);
+
+position1 ++;
+}
+}
+
+
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position= -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        
+        index ++;
+    }
+    return position;
+}
+
+
+
+int [] array = new int [10];
+
+Fillarray (array);
+Printarray (array);
+Console.WriteLine();
+int pos = Index0f(array, 994);
+Console. WriteLine (pos);
+
+
+
