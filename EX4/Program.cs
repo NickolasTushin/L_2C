934580b51bcb6ad149f915ed507d23adeca4adef
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-01 18:03:06 +0300

    EX4array index

---
+++ EX4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Имеется одномерный массив array из п элементов, требуется найти элемент массива, равный find
+//Установить счетчик index в позицию 0
+//Если array [index] = find, алгоритм завершил работу успешно.
+//Увеличить index на 1 Если index < n, то перейти к шагу 2. В противном случае
+//алгоритм завершил работу безуспешно.
+
+int[] array = {1, 2, 8, 4, 5, 6, 7, 8};
+int n = array.Length;
+int find = 8;
+int index = 0;
+while (index < n)
+{
+if (array[index]==find)
+{
+   Console.WriteLine(index);
+   break; 
+}
+index ++;
+}
