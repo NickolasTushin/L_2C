bfeeb0a8964c6bfb440ef77abccfbd9120586cda
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-01 17:37:35 +0300

    EX3array

--- EX3array/Program.cs
+++ EX3array/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int Max(
+    int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2> result) result = arg2;
+if (arg3> result) result = arg3;
+return result;
+}
+int [] array = { 15, 72, 56, 44, 8, 76, 17, 58, 19,};
+
+//int max1 = Max(a1,b1,c1);
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
+int max = Max(
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7],array[8]));
+
+Console.WriteLine(max);
